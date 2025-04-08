a51a32cf21c1678e3be11f0772837f35712b76e8
贾西贝 <11843487+jiaxibei62@user.noreply.gitee.com>
2025-04-08 16:37:31 +0800

    支委信息编辑

--- src/http/index.js
+++ src/http/index.js
@@ -57,7 +57,6 @@ instance.interceptors.request.use((config) => {
 instance.interceptors.response.use((response) => {
     hideLoading()
     //响应成功
-    //console.log('拦截器报错');
     return response.data;
 }, (error) => {
     console.log(error)
@@ -65,50 +64,59 @@ instance.interceptors.response.use((response) => {
     // 请求失败也应该取消Loading
     hideLoading()
 
-    let message=""
+    let message = ""
     //响应错误
-    if(error.response&&error.response.status){
-     const status = error.response.status
-      switch (status) {
-          case 400:
-              message = '请求错误';
-              break;
-          case 401:
-              message = '请求错误';
-              break;
-          case 403:
-              message = '未授权, 需要更高的权限';
-              break;
-          case 404:
-              message = '请求地址出错';
-              break;
-          case 408:
-              message = '请求超时';
-              break;
-          case 500:
-              message = '服务器内部错误!';
-              break;
-          case 501:
-              message = '服务未实现!';
-              break;
-          case 502:
-              message = '网关错误!';
-              break;
-          case 503:
-              message = '服务不可用!';
-              break;
-          case 504:
-              message = '网关超时!';
-              break;
-          case 505:
-              message = 'HTTP版本不受支持';
-              break;
-          default:
-              message = '请求失败'
-      }
+    if(error.response && error.response.status){
+        const status = error.response.status
+        
+        // 检查后端是否返回了详细错误信息
+        if (error.response.data && error.response.data.message) {
+            // 使用后端返回的具体错误信息
+            message = error.response.data.message;
+        } else {
+            // 如果没有具体错误信息，使用默认提示
+            switch (status) {
+                case 400:
+                    message = '请求错误';
+                    break;
+                case 401:
+                    message = '请求错误';
+                    break;
+                case 403:
+                    message = '未授权, 需要更高的权限';
+                    break;
+                case 404:
+                    message = '请求地址出错';
+                    break;
+                case 408:
+                    message = '请求超时';
+                    break;
+                case 500:
+                    message = '服务器内部错误!';
+                    break;
+                case 501:
+                    message = '服务未实现!';
+                    break;
+                case 502:
+                    message = '网关错误!';
+                    break;
+                case 503:
+                    message = '服务不可用!';
+                    break;
+                case 504:
+                    message = '网关超时!';
+                    break;
+                case 505:
+                    message = 'HTTP版本不受支持';
+                    break;
+                default:
+                    message = '请求失败'
+            }
+        }
+        
         ElMessage.error(message);
         return Promise.reject(error);
-       }
+    }
     return Promise.reject(error);
 });
 
